0d41528eed2857e36f9c3d4f5a2fb22b3ccb95ac
harryfh <128628952+harryfh@users.noreply.github.com>
2024-01-03 00:33:39 -0800

    Changed Buttons, added Resume, fixed links Added padding to sections for readability Changerd button colors to background to fit theme Fixed Links

--- Personal Portfolio/js/script.js
+++ Personal Portfolio/js/script.js
@@ -23,3 +23,58 @@ containers.forEach(container => {
     container.style.setProperty('--position', `${e.target.value}%`);
   })
 })
+
+const allLinks = document.querySelectorAll("a")
+const mybutton = document.getElementById("goBack")
+allLinks.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+       
+      
+        const href = link.getAttribute("href")
+        if (href === "#" || href.startsWith("#") ) {
+            e.preventDefault();
+        }
+      
+        
+        console.log(href)
+         // Scroll To Top
+        if (href === "#") 
+        
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+        });
+
+        // Scroll Else
+        if (href !== "#" && href.startsWith("#")) {
+            
+            const sectionElement = document.querySelector(href);
+            sectionElement.scrollIntoView({behavior: "smooth"});
+        }
+       
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.opacity = "1";
+        } else {
+            mybutton.style.opacity = "0";
+        }
+    
+            
+    });
+});
+function checkFlexGap() {
+    var flex = document.createElement("div");
+    flex.style.display = "flex";
+    flex.style.flexDirection = "column";
+    flex.style.rowGap = "1px";
+  
+    flex.appendChild(document.createElement("div"));
+    flex.appendChild(document.createElement("div"));
+  
+    document.body.appendChild(flex);
+    var isSupported = flex.scrollHeight === 1;
+    flex.parentNode.removeChild(flex);
+    console.log(isSupported);
+  
+    if (!isSupported) document.body.classList.add("no-flexbox-gap");
+  }
+  checkFlexGap();
